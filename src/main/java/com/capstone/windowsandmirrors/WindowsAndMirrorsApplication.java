b37d5c1006a33a17207d23b94a3fa0ba60b04540
Mariya Burrows <mariya.burrows@gmail.com>
2019-12-30 22:32:53 -0800

    update application.properties to connect to book_library database

---
+++ src/main/java/com/capstone/windowsandmirrors/WindowsAndMirrorsApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.windowsandmirrors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class WindowsAndMirrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsAndMirrorsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capstone/windowsandmirrors/WindowsAndMirrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.windowsandmirrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowsAndMirrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

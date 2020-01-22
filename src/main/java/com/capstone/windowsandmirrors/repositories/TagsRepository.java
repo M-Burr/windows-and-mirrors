f2459f42995ac522050b4141f60545ec3376ebcc
Mariya Burrows <mariya.burrows@gmail.com>
2020-01-21 21:52:47 -0800

    sql lowercase

--- src/main/java/com/capstone/windowsandmirrors/repositories/TagsRepository.java
+++ src/main/java/com/capstone/windowsandmirrors/repositories/TagsRepository.java
@@ -1,10 +1,15 @@
 package com.capstone.windowsandmirrors.repositories;
 
 import com.capstone.windowsandmirrors.models.Tag;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagsRepository extends CrudRepository<Tag, Long> {
+    @Query(
+            value = "select * from tags where lower(identifier)=lower(?1)",
+            nativeQuery = true
+    )
     Tag findTagByIdentifier(String identifier);
 }
